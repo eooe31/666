cd03ef802f313ee6c03788300822a20a4d74f73c
@FF7FS <120066595+eooe31@users.noreply.github.com>
2023-11-09 13:59:26 +0300

    Add files via upload

--- checktele.py
+++ checktele.py
@@ -142,14 +142,14 @@ def gen_user(choice):
             pass
     if choice == "8":
         c = d = random.choices(a)
-        d = random.choices(e)
-        f = [c[0], d[0], d[0], c[0], c[0], d[0]]
+        d = random.choices(b)
+        f = [c[0], d[0], d[0], d[0], c[0], d[0]]
         random.shuffle(f)
         username = ''.join(f)
         if username in banned[0]:
             c = d = random.choices(a)
-            d = random.choices(e)
-            f = [c[0], d[0], d[0], c[0], c[0], d[0]]
+            d = random.choices(b)
+            f = [c[0], d[0], d[0], d[0], c[0], d[0]]
             random.shuffle(f)
             username = ''.join(f)
         else:
